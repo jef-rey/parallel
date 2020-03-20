2b813898b66cb642d8a3aae532e57e995d4c9858
jeff <j3ff.anderson@gmail.com>
2020-03-19 22:47:53 -0700

    working on getting same result repeated. currently not

--- p3/tmin.c
+++ p3/tmin.c
@@ -53,35 +53,58 @@ long double arte(long double approx_value){
   return result;
 
 }
+void print_report(long double approx, long double abs_rel_tru_error){
 
-void report(long double a, long double d, int t){
+  //printf("the integration result = %.13Le\n", approx);
+  printf("the integration result = %.25Le\n", approx);
+  //USING Le INSTEAD OF e AS e ONLY RESULTS IN WARNINGS WHEN USING LONG DOUBLES
+  printf("the absolute relative true error is = %.19Le\n", abs_rel_tru_error);
+
+}
+void report(long double a, long double b, int t){
 
   long double approx = approx_area(a, b, t);
   long double abs_rel_tru_error = arte(approx); 
-  printf("the integration result = %.13Le\n", approx);
-  //USING Le INSTEAD OF e AS e ONLY RESULTS IN WARNINGS WHEN USING LONG DOUBLES
-  printf("the absolute relative true error is = %.19Le\n", abs_rel_tru_error);
+  print_report(approx, abs_rel_tru_error);
+  //  printf("the integration result = %.13Le\n", approx);
+  //  //USING Le INSTEAD OF e AS e ONLY RESULTS IN WARNINGS WHEN USING LONG DOUBLES
+  //  printf("the absolute relative true error is = %.19Le\n", abs_rel_tru_error);
 }
 
 int find_UB(long double a, long double b, int t){
+  printf("trying t at : %d\n", t);
   long double stop_criteria = 0.5E-14;
-  while(arte(approx_area(a, b , t) > stop_criteria)){
+  long double t_approx_area = approx_area(a,b,t);
+  long double t_arte = arte(t_approx_area);
+  print_report(t_approx_area, t_arte);
+  while(t_arte > stop_criteria){
     // if this returns true, then we have _an_ upperbound.
     t *= 2;
+    printf("trying t at : %d\n", t);
+    t_approx_area = approx_area(a,b,t);
+    t_arte = arte(t_approx_area);
+    print_report(t_approx_area, t_arte);
     // doubling t to get that upper bound. 
   }
 
   return t;
 }
 
+int binary_search(long double a, long double b, int lower_bound, int upper_bound );
+
 void search(long double a, long double b, int t){
 
   int upper_bound = find_UB(a, b, t);
   int lower_bound = 1;
+  int tmin = 1;
 
   if (t < 1){
     fprintf(stderr, "t cannot be < 1 ");
   }
+  tmin = binary_search(a, b, lower_bound, upper_bound); 
+  // this should return tmin
+
+  printf("tmin is: %d\n", tmin);
 
 }
 
@@ -89,25 +112,34 @@ int binary_search(long double a, long double b, int lower_bound, int upper_bound
 
   long double stop_criteria = 0.5E-14;
   int mid = lower_bound + (upper_bound - lower_bound ) / 2;
+  //long double mid = lower_bound + floorl((upper_bound - lower_bound ) / 2);
 
   if (upper_bound >= lower_bound){
     //do something
     //if the upper bound
-    if (mid == x) 
-      return mid; 
 
+    printf("trying t at : %d\n", mid);
+    long double mid_approx_area = approx_area(a,b,mid);
+    long double mid_arte = arte(mid_approx_area);
+
+    print_report(mid_approx_area, mid_arte);
+
+    if (mid_arte <= stop_criteria && arte(approx_area(a,b,mid - 1)) > stop_criteria){
+      return mid;
+    }
     //If element is smaller than mid, then 
     // it can only be present in left subarray 
-    if (arte(approx_area(a,b,mid)) <= stop_criteria){ 
+    if (mid_arte <= stop_criteria){ 
       // then mid is definitely not a valid answer
       return binary_search(a, b, lower_bound, mid); 
     }
-    // else the element can only be present in the R subarrahy
-      return binary_search(a, b, mid + 1, upper_bound);
+    // else the element can only be present in the R subarray
+    return binary_search(a, b, mid + 1, upper_bound);
 
   }
 
 
+  return -1; // error'd out. 
 
 }
 
@@ -150,15 +182,16 @@ int main(){
     report(a, b, t);
 
   }else if (input == 'S'){
+
     //stuff for S
+    printf("type in values for integration: lower bound, upper bound and number of [T]raps: ");
+    scanf("%LF %LF %d", &a, &b, &t);
+    search(a, b, t);
+
   }else{
     printf("I don't know what you mean.");
     exit (1);
   }
-
-
-
-
 
 }
 
@@ -180,5 +213,5 @@ int main(){
  *
  * ub = # of traps
  * 
- * is arte(approx_are(a, b, t)) <= stop_criteria
+ * is arte(approx_area(a, b, t)) <= stop_criteria
  */
