679865e30a70360bb6d141d0522005b5dd69f38b
jeff anderson <j3ff.anderson@gmail.com>
2020-05-08 19:53:17 -0700

    commented

--- p5/gepp.c
+++ p5/gepp.c
@@ -1,6 +1,9 @@
-// p5
-// Gaussian Elimination with Partial Pivoting
-//
+/* p5
+* Jeff Anderson
+* CSCI 551 - Gaussian Elimination with Partial Pivoting
+* May 2020
+*
+*/
 
 #include <math.h>
 #include <stdio.h>
@@ -8,6 +11,15 @@
 #include <time.h>
 #include <omp.h>
 
+/**
+ * \brief L2 norm computes the square root of the sum of the squares of the
+ * residual vector Ax-b
+ * \params n, the size of the matrix
+ *         A, the LHS of the matrix, filled with random variables
+ *         b, the RHS of the matrix, here as a vector of answers
+ *         x, the unknowns
+ * \return the sum of the squares of the residual vector Ax-b
+ */
 double L2norm(int n, double* A, double* b, double* x){
 
   double Ax[n];
@@ -30,14 +42,23 @@ double L2norm(int n, double* A, double* b, double* x){
   return sqrt(squares_sum);
 }
 
+/**
+ * \brief function to help with partial pivoting in gaussian elimination
+ * \decription we use pivoting to swap the current row with the row with the
+ * largest absolute value in the column of interest. this technique avoids
+ * division by zero errors and reduces round off errors
+ * \params n, the size of the matrix
+ *         A, the LHS of the matrix, filled with random variables
+ *         b, the RHS of the matrix, here as a vector of answers
+ *         diag, the [i][j] position that we are looking at
+ * \return void function
+ */
 void pivot(int n, double* A, double* b, int diag ){
   int i;
-  
+
   double greatest_val = 0;
   int row_with_greatest_elem = diag;
-  
-/*#pragma omp parallel for num_threads(thread_count) \
-  private(i) shared(n, A, b, diag) */
+
   for (/*int*/ i = diag; i < n; i++){
     double abs_elem = fabs(A[i*n+diag]);
     if (abs_elem > greatest_val){
@@ -47,8 +68,6 @@ void pivot(int n, double* A, double* b, int diag ){
   }
 
   //swap rows in A column
-/*#pragma omp parallel for num_threads(thread_count) \
-  private(i) shared(n, A, b, diag)*/
   for (/*int*/ i = diag; i < n; i++){
     double tmp = A[diag*n+i];
     A[diag*n+i] = A[row_with_greatest_elem*n+i];
@@ -59,10 +78,20 @@ void pivot(int n, double* A, double* b, int diag ){
   temp = b[diag] ;
   b[diag] = b[row_with_greatest_elem];
   b[row_with_greatest_elem] = temp;
-  
+
 
 }
 
+/**
+ * \brief forward_elim
+ * \decription transforms the matrix A into the _row echelon form_ wherein the
+ * (i,j)th term is reduced to 0,  and all entries in a column below the leading
+ * coefficient are zeros.
+ * \params n, the size of the matrix
+ *         A, the LHS of the matrix, filled with random variables
+ *         b, the RHS of the matrix, here as a vector of answers
+ * \return void function
+ */
 void forward_elim(int n, double* A, double* b ){
   //want to solve for the x[n-1] variable and the A[n-1] row
 
@@ -72,9 +101,9 @@ void forward_elim(int n, double* A, double* b ){
     pivot(n, A, b, i);
 #   pragma omp parallel for 
     for (int j = i+1; j < n; j++){
-//      if(omp_get_thread_num() == 0){
-//        printf("number of threads = %d\n", omp_get_num_threads());
-//            }
+      //      if(omp_get_thread_num() == 0){
+      //        printf("number of threads = %d\n", omp_get_num_threads());
+      //            }
       double scaling_factor = (A[j*n+i] / A[i*n+i]);
       for(int k = i; k < n; k++){
         //everything to the left of the ith item is eliminated already
@@ -85,9 +114,24 @@ void forward_elim(int n, double* A, double* b ){
     //gauss
     //use row[i] to eliminate variable[1][2] in all rows > i
   }
-
 }
 
+/**
+ * \brief back substituion
+ * \decription we can assume at this point that the matrix we are dealing with
+ * has been swapped/pivoted correctly and is now in the Upper Triangular form,
+ * meaning that the matrix would have the variables in this form:
+ *                    [ x  x  x ]
+ *                    [ 0  x  x ]
+ *                    [ 0  0  x ]
+ *  once it is that form, we can solve the x_3 value  and then work backward to
+ *  solve for all the unknowns
+ * \params n, the size of the matrix
+ *         A, the LHS of the matrix, filled with random variables
+ *         b, the RHS of the matrix, here as a vector of answers
+ *         x, a vector of unknowns
+ * \return void function
+ */
 void back_substitution(int n, double* A, double* b, double* x){
 
   //backward loop
@@ -96,7 +140,7 @@ void back_substitution(int n, double* A, double* b, double* x){
     double new_b = b[i];
     for (int j = n-1; j >= i; j--){
       // j = i === diagonal, which will have nonzero values
-      // shifting the values of A that are not on the diagonal over to B to
+      // shifting the values of A that are not on the diagonal over to b to
       // solve for x
       if (i == j){
         x[i] = new_b / A[i*n+j];
@@ -106,25 +150,33 @@ void back_substitution(int n, double* A, double* b, double* x){
   }
 }
 
+/**
+ * \brief main driver
+ * \description initializes variables, matrix, vectors. feeds to forward_elim
+ * and then back_substituion 
+ * \params command line arguments
+ * \return 0
+ */
 int main(int argc, char ** argv){
 
   int n;
   if (argc != 2){
     printf("./gepp <size of n>\n ");
-    // this is more a relic of testing, since we ran this using the sbatch
-    // system
     exit(1);
   }
 
 
   n = atoi(argv[1]);
-  
+
 
   double start, end;
 
   start = omp_get_wtime();
 
   srand48(time(0));
+
+  int num_procs = omp_get_num_procs();
+  printf("num procs = %d\n", num_procs);
 
   double *A;
   double *A_orig;
